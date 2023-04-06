0974383aa296c12549b707b8efac43ac2ed278a0
Igor Kostyakov <i.a.kostyakov@gmail.com>
2023-04-06 23:33:17 +0300

    init

---
+++ src/modules/_root/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  const messages = [
+    {
+      id: 1,
+      text: '123',
+    },
+  ];
+  const actions = [
+    {
+      id: 1,
+      text: 'qwe',
+    },
+  ]
+</script>
+
+<template>
+  <div class="main">
+    <div class="messages">
+      <div v-for="message in messages" :key="message.id" class="message">{{ message.text }}</div>
+    </div>
+    <div class="actions">
+      <div v-for="action in actions" :key="action.id" class="action">{{ action.text }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
